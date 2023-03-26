84b572522212b6e49da359651b7c5147cdb9d52a
Lucas Mateus <lmateus1067@outlook.com>
2023-03-26 19:29:07 -0300

    desafios e parte 4 completa

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -12,14 +12,24 @@ admin.site.register(Genre)
 # Register the Admin classes for Book using the decorator
 
 
+class BookInline(admin.TabularInline):
+    model = Book
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back')
 
     fieldsets = (
         (None, {
@@ -40,6 +50,8 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name',
                     'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
 
 
 admin.site.register(Author, AuthorAdmin)
