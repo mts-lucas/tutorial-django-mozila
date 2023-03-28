9212b17928d40be44ad722bdee741f5a71d9574c
Lucas Mateus <lmateus1067@outlook.com>
2023-03-28 12:34:55 -0300

    add parte 7

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -15,11 +15,15 @@ def index(request):
 
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
